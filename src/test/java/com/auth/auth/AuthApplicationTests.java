b4200abe1086f24ea54d0caf26cf3fbca4796454
herickteixeira <herickratai@gmail.com>
2022-10-28 12:42:04 -0300

    fix: ajusted compilation errors

--- src/test/java/com/auth/auth/AuthApplicationTests.java
+++ src/test/java/com/auth/auth/AuthApplicationTests.java
@@ -1,13 +1,7 @@
 package com.auth.auth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
